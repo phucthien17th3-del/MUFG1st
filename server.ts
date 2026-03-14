1a72515be192a871eb458f64520c18c29ce44f20
phucthien17th3 <phucthien17th3@gmail.com>
2026-03-14 15:22:40 +0700

    fix dist path render

--- server.ts
+++ server.ts
@@ -166,14 +166,14 @@ Sitemap: https://mufg-server.onrender.com/sitemap.xml`);
     });
     app.use(vite.middlewares);
   } else {
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.join(process.cwd(), "dist")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(process.cwd(), "dist/index.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(process.cwd(), "dist/index.html"));
 });
   }
 
