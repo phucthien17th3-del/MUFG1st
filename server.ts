06e6c0e0f76595c3da56f49862f882dee8ec5501
phucthien17th3 <phucthien17th3@gmail.com>
2026-03-14 12:25:44 +0700

    fix render root route

--- server.ts
+++ server.ts
@@ -159,6 +159,10 @@ async function startServer() {
   } else {
    app.use(express.static(path.join(__dirname, "../dist")));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
