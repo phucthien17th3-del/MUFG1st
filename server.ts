96fd2898b0f38c5c413d70776c28b196b31a92d5
phucthien17th3 <phucthien17th3@gmail.com>
2026-03-14 15:09:34 +0700

    fix vite express deploy

--- server.ts
+++ server.ts
@@ -173,7 +173,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "dist/index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
   }
 
