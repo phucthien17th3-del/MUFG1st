bf88b26ad028424fe996c41fb7f169aecf012126
phucthien17th3 <phucthien17th3@gmail.com>
2026-03-14 11:36:28 +0700

    fix cannot get root

--- server.ts
+++ server.ts
@@ -158,6 +158,10 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
   }
 
   // Socket.io logic
